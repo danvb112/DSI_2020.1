197317df17665bfa69733f92a86181419c991856
danvb112 <danielbonasser@gmail.com>
2021-05-11 10:13:52 -0300

    Entrega topico 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,168 +1,81 @@
-import 'package:english_words/english_words.dart';
+import 'package:dsi_app/view/word_pair_view.dart';
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-import 'package:dsi_2020_1/editPage.dart';
-
-Map<WordPair, bool> wordPairs;
-
+///TPC-2 (branch_wordPairs1):
+///Este app foi baseado no tutorial do Flutter disponível em:
+///https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1
+///
+/// Ele foi adaptado para utilizar o [BottomNavigationBar] exibindo listagens de
+/// palavras que o usuário gosta e desgosta. Para armazenar se o usuário gostou,
+/// desgostou, ou simplesmente não fez nada com a palavra, este app utiliza um
+/// [Map] denominado [wordPairs]. Este mapa, equivalente ao dicionário do Python,
+/// armazena o par de palavras como chave e um valor do tipo [bool], indicando
+/// se o usuário gostou ou desgostou da palavra. Caso seja null, indica que o
+/// usuário não gostou nem desgostou da palavra.
+///
+/// Vale frisar, que este mapa foi criado usando Generics. Assim, caso o
+/// programador tente colocar uma chave diferente do tipo [WordPair] ou um valor
+/// que não seja do tipo [bool], ocorrerá um erro de compilação.
+/// Para maiores informações, acesse:
+/// https://dart.dev/guides/language/language-tour#generics
+///
+/// Este código também inclui um getter para os itens deste mapa (vide [items]).
+/// Para maiores informações acesse:
+/// https://dart.dev/guides/language/language-tour#getters-and-setters
+///
+///TPC-3 (branch_wordPairs2):
+/// Este app foi ajustado para usar uma classe própria ([DSIWordPair]), ao invés
+/// de usar o [WordPair] diretamente. Assim, novos atributos podem ser
+/// adicionados na classe. Assim, ao invés de utilizar o Map para armazenar
+/// a informação da curtida, esta informação fica em um atributo da própria
+/// classe.
+///
+/// Esta classe sobrescreve dois métodos importantes: o [DSIWordPair#toString]
+/// e o [DSIWordPair#compareTo]. O primeiro converte um objeto desta classe em
+/// uma string (evitando a necessidade da função [asString] criada no tópico
+/// anterior. Já o segundo, compara um objeto da classe com outro, permitindo
+/// o conceito de ordenamento, utilizado no método [List#sort].
+///
+/// Esta atualização também ajusta a linha da listagem para diferenciar o botão
+/// de curtida e a linha.
+///
+///TPC-3 (branch_wordPairs3):
+/// Esta atualização inclui a parte de rotas, que permite navegar entre as telas
+/// em um App Flutter. Além disso, é criada a tela de atualização do par de palavras.
+/// Acesse: https://flutter.dev/docs/cookbook/navigation/named-routes
+///
+///TPC-4 (branch_wordPairs4):
+/// Esta atualização ajusta o código para utilizar os padrões
+/// MVC (Model-View-Controller) e Singleton.
+/// Acesse:
+/// https://pt.wikipedia.org/wiki/MVC
+/// https://pt.wikipedia.org/wiki/Singleton
 void main() {
-  initWordPairs();
   runApp(DSIApp());
 }
 
-void initWordPairs() {
-  final generatedWordPairs = generateWordPairs().take(20);
-  wordPairs =
-      Map.fromIterable(generatedWordPairs, key: (e) => e, value: (e) => null);
-}
-
-///App baseado no tutorial do Flutter disponível em:
-///https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1
+///Classe principal que representa o App.
 class DSIApp extends StatelessWidget {
+  ///Constrói o App e suas configurações.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'App de Listagem - DSI/BSI/UFRPE',
+      title: 'DSI App (BSI UFRPE)',
       theme: ThemeData(
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
+      initialRoute: HomePage.routeName,
+      routes: _buildRoutes(context),
     );
   }
-}
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int pageIndex = 0;
-  List<Widget> _pages = [
-    RandomWordsListPage(null),
-    RandomWordsListPage(true),
-    RandomWordsListPage(false)
-  ];
-
-  void _changePage(int value) {
-    setState(() {
-      pageIndex = value;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('App de Listagem - DSI/BSI/UFRPE'),
-      ),
-      body: _pages[pageIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: _changePage,
-        currentIndex: pageIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.thumb_up_outlined),
-            label: 'Liked',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.thumb_down_outlined),
-            label: 'Disliked',
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class RandomWordsListPage extends StatefulWidget {
-  final bool _filter;
-  RandomWordsListPage(this._filter);
-
-  @override
-  _RandomWordsListPageState createState() => _RandomWordsListPageState();
-}
-
-///Esta classe é o estado da classe que lista os pares de palavra
-///
-class _RandomWordsListPageState extends State<RandomWordsListPage> {
-  final _icons = {
-    null: Icon(Icons.thumbs_up_down_outlined),
-    true: Icon(Icons.thumb_up, color: Colors.blue),
-    false: Icon(Icons.thumb_down, color: Colors.red),
-  };
-
-  Iterable<WordPair> get items {
-    if (widget._filter == null) {
-      return wordPairs.keys;
-    } else {
-      //a linah abaixo retorna os pares filtras opelo filtro
-      return wordPairs.entries
-          .where((element) => element.value == widget._filter)
-          .map((e) => e.key);
-    }
-  }
-
-  _toggle(WordPair wordPair) {
-    bool like = wordPairs[wordPair];
-    if (widget._filter != null) {
-      wordPairs[wordPair] = null;
-    } else if (like == null) {
-      wordPairs[wordPair] = true;
-    } else if (like == true) {
-      wordPairs[wordPair] = false;
-    } else {
-      wordPairs[wordPair] = null;
-    }
-    setState(() {});
-  }
-
-  String capitalize(String s) {
-    return '${s[0].toUpperCase()}${s.substring(1)}';
-  }
-
-  String asString(WordPair wordPair) {
-    return '${capitalize(wordPair.first)} ${capitalize(wordPair.second)}';
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16),
-        itemCount: items.length * 2,
-        itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd) {
-            return Divider();
-          }
-          final int index = i ~/ 2;
-          return _buildRow(index + 1, items.elementAt(index));
-        });
-  }
-
-  Widget _buildRow(int index, WordPair wordPair) {
-    return ListTile(
-        title: Text('$index. ${asString(wordPair)}'),
-        trailing: IconButton(
-          icon: _icons[wordPairs[wordPair]],
-          onPressed: () => _toggle(wordPair),
-        ),
-        onLongPress: () =>
-            {print(wordPair), wordPairs.remove(wordPair), setState(() {})},
-        onTap: () async {
-          final newPair = await Navigator.push(context,
-              MaterialPageRoute(builder: (context) => EditPage(wordPair)));
-          if (newPair == null) {
-            return null;
-          }
-          print(newPair);
-          setState(() {
-            wordPairs[wordPair] = wordPairs[newPair];
-          });
-        });
+  ///Método utilizado para configurar as rotas.
+  Map<String, WidgetBuilder> _buildRoutes(BuildContext context) {
+    return {
+      WordPairUpdatePage.routeName: (context) => WordPairUpdatePage(),
+    };
   }
 }
