1acf481473c9a70df48172ae545456db6681c274
danvb112 <danielbonasser@gmail.com>
2021-05-21 10:55:48 -0300

    Topico 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,36 @@
+import 'package:dsi_app/view/word_pair_view.dart';
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
-  runApp(MyApp());
+  // inicializa o firebase
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(DSIApp());
 }
 
-class MyApp extends StatelessWidget {
+///Classe principal que representa o App.
+class DSIApp extends StatelessWidget {
+  ///Constrói o App e suas configurações.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DSI App (BSI UFRPE)',
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
+      initialRoute: HomePage.routeName,
+      routes: _buildRoutes(context),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
-    );
+  ///Método utilizado para configurar as rotas.
+  Map<String, WidgetBuilder> _buildRoutes(BuildContext context) {
+    return {
+      WordPairUpdatePage.routeName: (context) => WordPairUpdatePage(),
+    };
   }
 }
